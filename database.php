fb634a7b4a3ab8d72af61feb10e40dcaceb46a94
LucasNava <navarrolucas4668@gmail.com>
2024-06-10 08:04:54 -0300

    cambios en las rutas de la bd

--- database.php
+++ database.php
@@ -1,10 +1,17 @@
 <?php
 
+// $connection = mysqli_connect(
+//     'localhost',
+//     'root',
+//     '',
+//     'tasks'
+// );
+
 $connection = mysqli_connect(
-    'localhost',
-    'root',
-    '',
-    'tasks'
+    'sql208.byethost17.com',
+    'b17_36692845',
+    'xtxriver4668',
+    'b17_36692845_tasks'
 );
 
     // if ($conexion){
